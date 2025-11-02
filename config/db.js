51dc6ec39639856302c60fa709a302b97048c6fb
Mer <101873028+CheaterMer@users.noreply.github.com>
2025-11-03 02:20:51 +0900

    Add MYSQL_PORT support

--- config/db.js
+++ config/db.js
@@ -4,10 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASS || process.env.DB_PASS,
-  database: process.env.MYSQL_DB || 'discord_ranks',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB,
+  port: process.env.MYSQL_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
